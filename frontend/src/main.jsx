27b6ec9a90f6e6e8650f7ff349ca4c4e86fbb3d4
ncq01112006 <nc.quang0111@gmail.com>
2025-11-30 16:45:59 +0700

    add favicon

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+
 import "./styles/reset.css";
 import "./styles/theme.css";
 import "./styles/responsive.css";
